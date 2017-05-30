9b964cc7efe5ed7eba00ec857ee0a4d8e4aa73d5
Light <sgsdxzy@gmail.com>
2017-05-31 00:52:32 +0800

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup (name = 'smartlink',
-    version='0.81',
+    version='0.82',
     description='Smartlink protocal and implementation',
     author='Light',
     author_email='sgsdxzy@gmail.com',
